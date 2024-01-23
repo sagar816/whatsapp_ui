88d59912d2178fb3cc54c7adf1b5e41584c8e5a6
sagart <sagart@gmail.com>
2024-01-24 00:38:28 +0530

    4 com

--- lib/models/chat_models.dart
+++ lib/models/chat_models.dart
@@ -32,7 +32,68 @@ List<ChatModel> chatData = [
     name: "Sumit",
     message: "suno bhai",
     time: "04:00",
-    avatar: "",
+    avatar: "images/p3.jpeg",
+  ),
+   ChatModel(
+    name: "saniya",
+    message: "happy Birthday",
+    time: "12:00",
+    avatar: "images/p4.jpeg",
+  ),
+   ChatModel(
+    name: "Rita",
+    message: "tomorrow is holday",
+    time: "02:40",
+    avatar: "images/p5.jpeg",
+  ),
+   ChatModel(
+    name: "yuga",
+    message: "hi hello",
+    time: "04:00",
+    avatar: "images/p6.jpeg",
+  ),
+   ChatModel(
+    name: "kartik",
+    message: "criket now",
+    time: "03:30",
+    avatar: "images/p7.jpeg",
+  ),
+   ChatModel(
+    name: "raj",
+    message: "samsung laptop",
+    time: "04:30",
+    avatar: "images/p8.jpeg",
+  ),
+   ChatModel(
+    name: "vikas",
+    message: "come to office",
+    time: "11:00",
+    avatar: "images/p9.jpeg",
+  ),
+   ChatModel(
+    name: "pappu",
+    message: "vote me",
+    time: "05:00",
+    avatar: "images/p10.jpeg",
+  ),
+   ChatModel(
+    name: "orton",
+    message: "what time?",
+    time: "09:00",
+    avatar: "images/p11.jpeg",
+  ),
+   ChatModel(
+    name: "pranita",
+    message: "tution Time ?",
+    time: "08:00",
+    avatar: "images/p12.jpeg",
   ), // in real this data will come from database or API. 
+   ChatModel(
+    name: "akhil",
+    message: "come later",
+    time: "06:00",
+    avatar: "images/p13.jpeg",
+  ),
+
   // So idhar humne ChatModel naam ka bana diya class aur uska do object bana diya aur usko list me rakh diya
 ];
